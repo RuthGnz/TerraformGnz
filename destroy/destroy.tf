a1bfd229c4ef6714aa1829b886428072a0677c47
Ruthgnz <ruth.gonzalez@innovation4security.com>
2017-08-25 11:48:08 +0200

    dest3

--- destroy/destroy.tf
+++ destroy/destroy.tf
@@ -2,6 +2,12 @@ variable "instance_tag" {}
 variable "ACCESS_KEY" {}
 variable "SECRET_KEY" {}
 
+provider "aws" {
+  access_key = "${var.ACCESS_KEY}"
+  secret_key = "${var.SECRET_KEY}"
+  region     = "eu-west-1"
+}
+
 data "aws_instance" "created_instance" {
 
   filter {
